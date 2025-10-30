2b97980e3d53337ed5f64f203361ed527e984b90
CBRT513 <clif@barge2rail.com>
2025-10-30 18:32:53 -0400

    fix(migrations): indent dependencies/operations in 0005_auditlog and correct dependency name; should unblock deploy

--- bol_system/migrations/0005_auditlog.py
+++ bol_system/migrations/0005_auditlog.py
@@ -3,7 +3,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-dependencies = [
+    dependencies = [
         ('bol_system', '0004_product_chemistry_fields'),
     ]
 
